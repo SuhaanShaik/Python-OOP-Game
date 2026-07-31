f0a6b25eb6aae6abeea973418232aa08a22a6768
Suhaan Shaik <suhaan.f.shaik@gmail.com>
2026-07-30 17:56:12 -0700

    Create Achievement.py

---
+++ src/Achievement.py
@@ -0,0 +1,136 @@
+class Achievement:
+  def __init__(self, name, description):
+    self.name = name
+    self.description = description
+
+FIRST_WIN = Achievement(
+    name = "First Win",
+    description = "Win your first round."
+)
+
+ROOKIE = Achievement(
+    name = "Rookie",
+    description = "Win your first 10 rounds"
+)
+
+
+EXPERIENCED = Achievement(
+    name = "Experienced",
+    description = "Win your first 100 rounds across any difficulty"
+)
+
+VETERAN = Achievement(
+    name = "Veteran",
+    description = "Win your first 500 rounds across any difficulty"
+)
+
+LEGENDARY = Achievement(
+    name = "Legendary",
+    description = "Win your first 1000 rounds across any difficulty"
+)
+
+INSANITY = Achievement(
+    name = "Insanity",
+    description = "Win a round on Insane Difficulty"
+)
+
+MASTER = Achievement(
+    name = "Master",
+    description = "Win a round on every difficulty"
+)
+
+IMPOSSIBLE = Achievement(
+    name = "Impossible",
+    description = "Win every round on Super Easy Difficulty"
+)
+
+APPRENTICE = Achievement(
+    name = "Apprentice",
+    description = "Reach level 5"
+)
+
+WIZARD_OF_LUCK = Achievement(
+    name = "Wizard Of Luck",
+    description = "Reach level 20"
+)
+
+
+FORTUITOUS_WARLOCK = Achievement(
+    name = "Fortuitous Warlock",
+    description = "Reach level 50"
+)
+
+THE_CHAMPION = Achievement(
+    name = "The Champion",
+    description = "Reach level 100"
+)
+
+DEDICATED = Achievement(
+    name = "Dedicated",
+    description = "score 1000 lifetime points"
+)
+
+THE_BIGGEST_FISH = Achievement(
+    name = "The Biggest Fish",
+    description = "score 10,000 lifetime points"
+)
+
+WTF = Achievement(
+    name = "WTF",
+    description = "Win 10 rounds on Insane Difficulty"
+)
+
+SUPREME = Achievement(
+    name = "Supreme",
+    description = "Win 15 rounds on Super Hard Difficulty"
+)
+
+COURAGEOUS = Achievement(
+    name = "Courageous",
+    description = "Win 20 rounds on Hard Difficulty"
+)
+
+ON_FIRE = Achievement(
+    name="On Fire",
+    description="Reach a 5-win streak."
+)
+
+UNSTOPPABLE = Achievement(
+    name="Unstoppable",
+    description="Reach a 10-win streak."
+)
+
+BAD_LUCK = Achievement(
+    name = "Bad Luck",
+    description = "Lose your first round."
+)
+
+ROUGH_START = Achievement(
+    name = "Rough Start",
+    description = "Lose 10 rounds."
+)
+
+UNLUCKY = Achievement(
+    name = "Unlucky",
+    description = "Lose 100 rounds."
+)
+
+CURSED = Achievement(
+    name = "Cursed",
+    description = "Lose 500 rounds."
+)
+
+THE_BLACK_HOLE = Achievement(
+    name = "The Black Hole",
+    description = "Lose 1000 rounds."
+)
+
+WHY_ARE_YOU_HERE = Achievement(
+    name = "Why Are You Here?",
+    description = "Play 100 rounds and win less than 10%."
+)
+
+THE_COMEBACK = Achievement(
+    name = "The Comeback",
+    description = "Lose 50 rounds, then win 10 rounds in a row."
+)
